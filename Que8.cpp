c10c3c4de703a9bcbb3d940189d8fec9ef6b83fd
Sangam8168 <144759674+Sangam8168@users.noreply.github.com>
2024-01-04 02:44:52 +0530

    Create Que8.cpp

---
+++ Que8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            printf(" ");
+        }
+   for(int j=1;j<=1;j++){
+       printf("*");
+   }
+   printf("\n");
+}}
